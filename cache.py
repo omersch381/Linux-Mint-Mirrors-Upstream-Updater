ae20e3fce6fc5e3c6d08ffff2f1063df90b831b4
Guy Afik <gafik@redhat.com>
2021-02-16 00:22:09 +0200

    changed the cache file to be saved as a json

--- cache.py
+++ cache.py
@@ -1,56 +1,49 @@
+import json
 
 
 class CacheManager(object):
     """
     The purpose of this class is to manage the cache of the mirroring by editing the cache file for the mirrors.
+
+    Saves the data mirrors on the file as a json string.
     """
     def __init__(self, fastest_mirrors, cache_size=20):
         self._fastest_mirrors = fastest_mirrors
         self._cache_size = cache_size
+        self._cache_mirrors = {
+            mirror: time for (mirror, time), _ in zip(self._fastest_mirrors.items(), range(self._cache_size))
+        }
+
+    @property
+    def cache_mirrors(self):
+        return self._cache_mirrors
+
+    @cache_mirrors.setter
+    def cache_mirrors(self, cache_mirrors):
+        self._cache_mirrors = cache_mirrors
 
     def save(self):
         """
-        Saves the fastest mirror list into a file.
+        Saves the best mirrors into the cache according to the cache size.
         """
-
-        fastest_mirrors_names = []
-        for mirror in self._fastest_mirrors.keys():
-            if len(fastest_mirrors_names) >= self._cache_size:
-                break
-            fastest_mirrors_names.append(mirror)
-
         with open('mirrors_list', 'w') as file:
-            for mirror in fastest_mirrors_names:
-                file.write(f"{mirror}\n")
+            file.write(json.dumps(self._cache_mirrors))
 
     def load(self):
         """
-        Loads the fastest mirrors list from a file.
-
-        Returns:
-            list[str]: a list of all the fastest mirrors that are currently in the cache.
+        Loads the cache mirrors json from a file.
         """
-        mirrors_names = []
-
         with open('mirrors_list', 'r') as file:
-            for mirror in file:
-                mirrors_names.append(mirror.strip())
-                if len(mirrors_names) >= self._cache_size:
-                    return mirrors_names
+            self._cache_mirrors = json.load(file)
 
-# example to save a file.
-# CacheManager(
-#     fastest_mirrors={
-#         'walla.co.il': 0.004660946549847722, 'ynet.co.il': 0.05771557008847594, 'google.com': 0.05773383192718029
-#     },
-#     cache_size=1
-# ).save()
 
-# example to load a file
-# mirrors = CacheManager(
+# example to save & load.
+# cache = CacheManager(
 #     fastest_mirrors={
 #         'walla.co.il': 0.004660946549847722, 'ynet.co.il': 0.05771557008847594, 'google.com': 0.05773383192718029
 #     },
 #     cache_size=2
-# ).load()
-# print(mirrors)
+# )
+# cache.save()
+# cache.load()
+# print(cache.cache_mirrors)
