72509e5adc138018f7e6dd244de06a81f545aceb
Omer <omersch381@gmail.com>
2021-03-03 19:56:45 +0200

    Print statements were added

--- best_mirrors.py
+++ best_mirrors.py
@@ -8,6 +8,7 @@ class FastestMirrors(object):
     Attributes:
         _sorted_mirrors (dict): a sorted dict of mirrors
     """
+
     def __init__(self):
         self._sorted_mirrors = None
 
@@ -28,11 +29,19 @@ class FastestMirrors(object):
             {'walla.co.il': 0.004660946549847722, 'ynet.co.il': 0.05771557008847594, 'google.com': 0.05773383192718029}
         """
         mirrors_ping_average = {}
+        fastest_mirror_pinging_time = 2
 
         for mirror in mirrors:
             try:
+                print('Pinging {mirror}... '.format(mirror=mirror), end='')
                 ping_response = ping(target=mirror)
                 mirrors_ping_average[mirror] = ping_response.rtt_avg
+                print('took {avg_pinging_time} seconds.'.format(avg_pinging_time=mirrors_ping_average[mirror]))
+                if fastest_mirror_pinging_time > mirrors_ping_average[mirror]:
+                    fastest_mirror_pinging_time = mirrors_ping_average[mirror]
+                    print('{new_fastest_mirror} is the fastest mirror so far!'.format(
+                        new_fastest_mirror=mirror))
+
             except RuntimeError:  # means that mirror cannot be reached or broken.
                 pass
 
